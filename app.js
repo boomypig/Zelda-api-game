fa02ea83ddb51fcd13c0bfa6b80ab52125ef929b
Gavin Talbot <d00483135@CMP-SCC112-1111.local>
2024-06-14 10:50:12 -0600

    test

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ Vue.createApp({
     methods: {
         getMonsters: async function() {
             let response = await fetch ("https://botw-compendium.herokuapp.com/api/v3/compendium/category/monsters")
-            let data = await response.json()
+            let data = await response.json();
             this.monsters = data;
             console.log(this.monsters)
 
         }
 
     },
+
 }).mount('#app');
